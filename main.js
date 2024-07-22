784595be3a67f420de6449ee0861f77719ff0d14
Prayskey Ogbonna <prayskeyo@gmail.com>
2024-07-22 05:06:23 +0100

    modifiy main js file

--- main.js
+++ main.js
@@ -694,15 +694,13 @@ function firstUniqChar(s){
 
   for(let char of s){
     charCount[char] = (charCount[char] || 0) + 1;
-    console.log(charCount[char])
   }
-  
+
   for(let i = 0; i < s.length; i++){
     if(charCount[s[i]] === 1){
       return s[i]
     }
-    return -1
   }
+      return -1
 }
 
-console.log(firstUniqChar("finnaly"))
